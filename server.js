2bd363ce82c784ef949e914a194a6fc8d795d8c9
t-abhishek2.shukla <t-abhishek2.shukla@paytmmall.com>
2018-04-14 20:54:56 +0530

    Upated project by udemy

--- server.js
+++ server.js
@@ -1,82 +1,56 @@
-const express = require('express')
-const hbs = require('hbs')
-const fs = require('fs')
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000;
+var app = express();
 
-var app = express()
-
-// Static server setup (2)
-app.use(express.static(__dirname + '/public'))
-
-// Set 'hbs' (3)
-app.set('view engine', 'hbs')
-
-// Register partials (4)
 hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-// register helper to 'hbs' (5)
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
-})
-hbs.registerHelper('screamIt', (text) => {
-    return text.toUpperCase()
-})
-
-// Midleware (6) - To track how our server is working
 app.use((req, res, next) => {
-    var now = new Date().toString()
-    var log = `${now}: ${req.method} ${req.url}`
-    console.log(log)
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err) {
-            console.log('Unable to append to server.log.')
-        }
-    })
-    next()
-})
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-// For middlelayer
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
 // app.use((req, res, next) => {
-//     res.render('maintenence.hbs', {
-//         pageTitle: 'Under Maintenence',
-//         message: 'Will be back soon. Thank you for visiting!!'
-//     })
-// })
+//   res.render('maintenance.hbs');
+// });
 
+app.use(express.static(__dirname + '/public'));
 
-// Http route handlers (1)
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('Hello Express!')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
 
-    // res.send('<h1>Hello Express</h1>')  
-
-    res.send({
-        name: 'Abhishek Shukla',
-        address: '153, II-E Nehru Nagar, Ghaziabad, UP 201001',
-        emailId: [
-            'ramabhi777@gmail.com',
-            'in.abhishek.shukla@gmail.com'
-        ]
-    })
-})
-
-var currentYear = new Date().getFullYear()
-// Using 'hbs' to render the data
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        pageTitle: 'About hbs page'
-    })
-})
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
 
-// Using 'hbs' to render the data
-app.get('/home', (req, res) => {
-    res.render('home.hbs', {
-        pageTitle: 'Home page title',
-        message: 'Welcome to home'
-    })
-})
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is up and ready on port ${port}.`)
-})
+  console.log(`Server is up on port ${port}`);
+});
